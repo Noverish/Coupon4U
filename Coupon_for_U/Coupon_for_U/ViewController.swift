433191164d7954b1bd48e1fd1588f248e646de18
rebeca970228 <rebeca970228@gmail.com>
2017-01-18 17:21:40 +0900

    add origin project folder

---
+++ Coupon_for_U/Coupon_for_U/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coupon_for_U
+//
+//  Created by cscoi021 on 2017. 1. 11..
+//  Copyright © 2017년 dan1002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
